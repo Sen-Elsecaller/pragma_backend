1610e16ba714de2d5708c866c526d5ce0006faf8
Loksz <juanhernandezr0075@gmail.com>
2025-12-05 18:26:32 -0300

    Cambios en la configuración general

--- config/settings.py
+++ config/settings.py
@@ -157,18 +157,17 @@ else:
 	DATABASES = {
 		'default': {
 			'ENGINE': 'django.db.backends.postgresql',
-			'NAME': os.environ.get('DB_NAME', 'railway'),
-			'USER': os.environ.get('DB_USER', 'postgres'),
-			'PASSWORD': os.environ.get('DB_PASSWORD', 'BkurrjOpNTnepNQAvfQqLANJGiITphwC'),
-			'HOST': os.environ.get('DB_HOST', 'centerbeam.proxy.rlwy.net'),
-			'PORT': os.environ.get('DB_PORT', '47459'),
+			'NAME': os.environ.get('DB_NAME'),
+			'USER': os.environ.get('DB_USER'),
+			'PASSWORD': os.environ.get('DB_PASSWORD'),
+			'HOST': os.environ.get('DB_HOST'),
+			'PORT': os.environ.get('DB_PORT'),
 			'CONN_MAX_AGE': 600,
 			'OPTIONS': {
-				'sslmode': 'disable',
+				'sslmode': os.environ.get('DB_SSLMODE', 'disable'),
 			},
 		}
 	}
-
 
 # ============================================
 # PASSWORD VALIDATION
